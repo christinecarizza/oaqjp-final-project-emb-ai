800b0017ad52f45054c5fa7f3ed8f346a9c37cdb
christinecarizza <christinecarizza@gmail.com>
2026-09-16 01:56:26 -0400

    task 3: format the output of the application

--- emotion_detection.py
+++ emotion_detection.py
@@ -7,4 +7,21 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=headers, timeout=10)
 
-    return response.text
+    # Convert response to json
+    response_dict = json.loads(response.text)
+
+    # Extract the required set of emotions
+    emotions = response_dict["emotionPredictions"][0]["emotion"]
+
+    # Get the emotion with the highest score
+    emotion_max_score = max(emotions, key=emotions.get)
+    
+    modified_emotions = {
+        'anger': emotions["anger"],
+        'disgust': emotions["disgust"],
+        'fear': emotions["fear"],
+        'joy': emotions["joy"],
+        'sadness': emotions["sadness"],
+        'dominant_emotion': emotion_max_score
+    }
+    return modified_emotions
